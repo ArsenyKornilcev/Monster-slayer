45da6357383dbd1305665d31e3d86b47ba6e156b
ArsenyKornilcev <76811297+ArsenyKornilcev@users.noreply.github.com>
2023-08-27 17:55:18 +0500

    add new func for increase round activities

--- app.js
+++ app.js
@@ -12,31 +12,32 @@ const app = {
 	},
 	methods: {
 		attackMonster() {
-			this.currentRound++;
 			const attackValue = getRandomValue(5, 12);
 			this.monsterHealth -= attackValue;
-			this.attackPlayer();
+            this.increaseRound();
 		},
 		attackPlayer() {
 			const attackValue = getRandomValue(8, 15);
 			this.playerHealth -= attackValue;
 		},
 		specialAttackMonster() {
-			this.currentRound++;
 			const attackValue = getRandomValue(10, 25);
 			this.monsterHealth -= attackValue;
-			this.attackPlayer();
+            this.increaseRound();
 		},
 		healPlayer() {
-			this.currentRound++;
 			const healValue = getRandomValue(9, 20);
             if(healValue + this.playerHealth >= 100) {
                 this.playerHealth = 100;
             } else {
                 this.playerHealth += healValue;
             }
-            this.attackPlayer();
+            this.increaseRound();
 		},
+        increaseRound() {
+            this.currentRound++;
+            this.attackPlayer();
+        }
 	},
 	computed: {
 		monsterBarStyles() {
